9b032a7a897dfc16aa351bb5d0d6da1df46a26e5
rC34o7 <76730905+rC34o7@users.noreply.github.com>
2021-03-25 17:15:40 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,23 @@
+class rope {
+    constructor(body1,body2,xoffset){
+        var options= {
+            bodyA:body1,
+            bodyB:body2,
+            stiffness:0.5,
+            length:300,
+        }
+    this.xoffset=xoffset
+        this.rope=Constraint.create(options);
+    World.add(world,this.rope)
+
+
+    }
+    display(){
+        var rpos = this.rope.bodyA.position
+var bpos = this.rope.bodyB.position 
+stroke("red")
+strokeWeight(5)
+
+line(rpos.x + this.xoffset,rpos.y,bpos.x,bpos.y);
+    }
+}
